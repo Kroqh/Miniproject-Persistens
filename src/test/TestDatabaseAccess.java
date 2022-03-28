c1d5a7c2a615b81cf92b594a7088ea411c4a3840
Krogh <37517255+Kroqh@users.noreply.github.com>
2022-03-28 12:04:36 +0200

    DB

---
+++ src/test/TestDatabaseAccess.java
@@ -0,0 +1,56 @@
+package test;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.*;
+import java.time.LocalDate;
+
+//import controllayer.ControlPayStation;
+//import controllayer.Currency;
+//import controllayer.IPayStation;
+//import controllayer.IReceipt;
+//import controllayer.IllegalCoinException;
+
+import db.*;
+
+
+//import static org.junit.Assert.*;
+
+/**
+ * Inspired by the book: Flexible, Reliable Software Henrik Bærbak Christensen:
+ * Flexible, Reliable Software. Taylor and Francis Group, LLC 2010
+ */
+
+public class TestDatabaseAccess {
+	
+	DBConnection con = null;
+
+
+	/** Fixture for pay station testing. 
+	 * @throws DataAccessException */
+	@Before
+	public void setUp() throws DataAccessException {
+		con = DBConnection.getInstance();
+	}
+	
+	
+	@Test
+	public void wasConnected() {
+		assertNotNull("Connected - connection cannot be null", con);
+		
+		con.disconnect();
+		boolean wasNullified = con == null;
+		assertTrue("Disconnected - instance set to null", wasNullified);
+			
+	}
+
+	
+	@After
+	public void cleanUp() {
+
+		con.disconnect();
+
+	}	
+
+}
