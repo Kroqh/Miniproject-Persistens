1a616eb0669a20ed9a905aaee24e602bda49676b
Mads Bach <FentoNqq@hotmail.com>
2022-03-29 19:39:03 +0200

    Product
    Pro ducks

---
+++ src/controller/ProductController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import db.ProductDB;
+import model.Product;
+
+public class ProductController {
+
+	private ProductDB productDB;
+	
+	public Product addProductByName(int name) {
+		return null;
+	}
+	
+}
