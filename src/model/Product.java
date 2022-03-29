9fd51a47c10258f7b5ebbc692d3a517b4880091e
Mads Bach <FentoNqq@hotmail.com>
2022-03-29 22:25:11 +0200

    Update Product.java

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,5 +1,69 @@
 package model;
 
 public class Product {
+	private String name; 
+	private String countryOfOrigin;
+	private int purchasePrice;
+	private int salesPrice;
+	private int rentPrice;
+	private int minStock;
+	
+	public Product(String name, String countryOfOrigin, int purchasePrice, int salesPrice, int rentPrice, int minStock) {
+		this.name = name;
+		this.countryOfOrigin = countryOfOrigin;
+		this.purchasePrice = purchasePrice;
+		this.salesPrice = salesPrice;
+		this.rentPrice = rentPrice;
+		this.minStock = minStock;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountryOfOrigin() {
+		return countryOfOrigin;
+	}
+
+	public void setCountryOfOrigin(String countryOfOrigin) {
+		this.countryOfOrigin = countryOfOrigin;
+	}
+
+	public int getPurchasePrice() {
+		return purchasePrice;
+	}
+
+	public void setPurchasePrice(int purchasePrice) {
+		this.purchasePrice = purchasePrice;
+	}
+
+	public int getSalesPrice() {
+		return salesPrice;
+	}
+
+	public void setSalesPrice(int salesPrice) {
+		this.salesPrice = salesPrice;
+	}
+
+	public int getRentPrice() {
+		return rentPrice;
+	}
+
+	public void setRentPrice(int rentPrice) {
+		this.rentPrice = rentPrice;
+	}
+
+	public int getMinStock() {
+		return minStock;
+	}
+
+	public void setMinStock(int minStock) {
+		this.minStock = minStock;
+	}
+	
+	
 }
